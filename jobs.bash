339f9bf9e3c134739bb90f9a4e1cfaaab034af28
haichenl <haichenl@andrew.cmu.edu>
2017-03-15 16:45:42 -0400

    jobs.bash double net fix

--- jobs.bash
+++ jobs.bash
@@ -4,10 +4,10 @@ python -i -u dqn_atari.py --env SpaceInvaders-v0 --output ./output --input_shape
 
 python -i -u dqn_atari.py --env SpaceInvaders-v0 --output ./output --input_shape 84 84 --replay_buffer_size 100000 --model_name linear --num_train 500000 --target_reset_interval 10000 | tee linear_memory.out
 
-python -i -u dqn_atari.py --env SpaceInvaders-v0 --output ./output --input_shape 84 84 --replay_buffer_size 100000 --model_name linear --num_train 500000 --target_reset_interval 10000 --double_net | tee linear_double.out
+python -i -u dqn_atari.py --env SpaceInvaders-v0 --output ./output --input_shape 84 84 --replay_buffer_size 100000 --model_name linear --num_train 500000 --target_reset_interval 10000 --double_net True | tee linear_double.out
 
 python -i -u dqn_atari.py --env SpaceInvaders-v0 --output ./output --input_shape 84 84 --replay_buffer_size 100000 --model_name dqn --num_train 500000 --target_reset_interval 10000 | tee dqn.out
 
-python -i -u dqn_atari.py --env SpaceInvaders-v0 --output ./output --input_shape 84 84 --replay_buffer_size 100000 --model_name dqn --num_train 500000 --target_reset_interval 10000 --double_net | tee dqn_double.out
+python -i -u dqn_atari.py --env SpaceInvaders-v0 --output ./output --input_shape 84 84 --replay_buffer_size 100000 --model_name dqn --num_train 500000 --target_reset_interval 10000 --double_net True | tee dqn_double.out
 
-python -i -u dqn_atari.py --env SpaceInvaders-v0 --output ./output --input_shape 84 84 --replay_buffer_size 100000 --model_name dueling --num_train 500000 --target_reset_interval 10000 | tee dqn_dueling.out
+python -i -u dqn_atari.py --env SpaceInvaders-v0 --output ./output --input_shape 84 84 --replay_buffer_size 100000 --model_name dueling --double_net True --num_train 500000 --target_reset_interval 10000 | tee dqn_dueling.out
